8b354e7af4956d9dbab39f3e2ad031ef8524102e
Juan Fernando <jfdelarosa@outlook.com>
2021-03-01 14:05:29 -0600

    Feat: basic login init

---
+++ pages/login.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+v-row(justify="center" align="center")
+  v-col(cols="12" sm="8" md="6")
+    v-card
+      v-card-title Login
+      v-card-text
+        v-text-field(v-model="login.email" placeholder="Correo" outlined)
+        v-text-field(v-model="login.password" placeholder="Contraseña" outlined)
+        v-btn(@click="doLogin") Login
+</template>
+
+<script>
+export default {
+  layout: 'full',
+  data: () => ({
+    login: {
+      email: '',
+      password: '',
+    },
+  }),
+  methods: {
+    async doLogin() {
+      try {
+        let response = await this.$auth.loginWith('local', { data: this.login })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  },
+}
+</script>

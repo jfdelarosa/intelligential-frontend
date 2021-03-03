b7aeac8e196f541ec94178dc9a0962ca0505d659
Juan Fernando <jfdelarosa@outlook.com>
2021-03-02 19:40:57 -0600

    feat: se crea un componente para time-ago

---
+++ components/TimeAgo.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  v-tooltip(bottom)
+    template(v-slot:activator="{ on, attrs }")
+      span(v-bind="attrs" v-on="on") {{currentDate}}
+    span {{currentDateRelative}}
+</template>
+
+<script>
+import { formatDistanceToNowStrict, formatRelative } from 'date-fns'
+
+export default {
+  props: {
+    datetime: {
+      required: false,
+      type: [String, Number],
+      default: Date.now(),
+    },
+  },
+  data: () => ({
+    interval: null,
+    currentDate: null,
+    currentDateRelative: null,
+  }),
+  methods: {
+    parse(datetime) {
+      this.currentDate = formatDistanceToNowStrict(datetime, {
+        addSuffix: true,
+      })
+      this.currentDateRelative = formatRelative(new Date(), datetime)
+    },
+  },
+  mounted() {
+    const datetime = new Date(this.datetime)
+
+    this.parse(datetime)
+
+    this.interval = setInterval(() => {
+      this.parse(datetime)
+    }, 1000)
+  },
+  destroyed() {
+    clearInterval(this.interval)
+  },
+}
+</script>

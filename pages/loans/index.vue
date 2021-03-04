21aede1a9493be14d4e8cc0276d3a7355009d3f3
Juan Fernando <jfdelarosa@outlook.com>
2021-03-04 12:29:51 -0600

    Feat Se añaden nuevas páginas

---
+++ pages/loans/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+v-card(outlined)
+    v-tabs(
+      v-model="tab"
+      background-color="primary"
+      dark
+      fixed-tabs
+      icons-and-text
+    )
+      v-tabs-slider
+
+      v-tab
+        | Solicitudes
+        v-icon mdi-view-list
+
+      v-tab
+        | En curso
+        v-icon mdi-calendar-month
+
+      v-tab
+        | Historial
+        v-icon mdi-history
+
+    v-tabs-items
+      AppLoanRequests(v-if="tab === 0")
+      AppLoanCurrent(v-if="tab === 1")
+      AppLoanHistory(v-if="tab === 2")
+
+</template>
+
+<script>
+import AppLoanRequests from '@/components/loans/AppLoanRequests'
+import AppLoanCurrent from '@/components/loans/AppLoanCurrent'
+import AppLoanHistory from '@/components/loans/AppLoanHistory'
+
+export default {
+  components: { AppLoanRequests, AppLoanCurrent, AppLoanHistory },
+  data: () => ({
+    books: [],
+    tab: 'tab-1',
+  }),
+  async fetch() {},
+  methods: {},
+}
+</script>
---
+++ pages/myloans/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+v-card(outlined)
+    v-tabs(
+      v-model="tab"
+      background-color="primary"
+      dark
+      fixed-tabs
+      icons-and-text
+    )
+      v-tabs-slider
+
+      v-tab
+        | Solicitudes pendientes
+        v-icon mdi-view-list
+
+      v-tab
+        | En curso
+        v-icon mdi-calendar-month
+
+      v-tab
+        | Historial
+        v-icon mdi-history
+
+    v-tabs-items
+      AppMyPendingLoans(v-if="tab === 0")
+      AppCurrentLoans(v-if="tab === 1")
+      AppMyHistory(v-if="tab === 2")
+
+</template>
+
+<script>
+import AppMyPendingLoans from '@/components/myloans/AppMyPendingLoans'
+import AppCurrentLoans from '@/components/myloans/AppCurrentLoans'
+import AppMyHistory from '@/components/myloans/AppMyHistory'
+
+export default {
+  components: { AppMyPendingLoans, AppCurrentLoans, AppMyHistory },
+  data: () => ({
+    books: [],
+    tab: 0,
+  }),
+  async fetch() {},
+  methods: {},
+}
+</script>

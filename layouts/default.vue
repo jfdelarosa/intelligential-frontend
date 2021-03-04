303f6c6efba249882923c7b1c3c4eccbd96274f1
Juan Fernando <jfdelarosa@outlook.com>
2021-03-04 12:29:29 -0600

    Fix: Cambios en el menú

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 v-app
   v-navigation-drawer(v-model="drawer" clipped fixed app)
     v-list
-      template(v-for="(item, i) in items")
+      template(v-for="(item, i) in menu")
         v-subheader(v-if="item.header") {{ item.header }}
         v-list-item(router exact :to="{ name: item.name }" v-else)
           v-list-item-action
@@ -10,7 +10,7 @@ v-app
           v-list-item-content
             v-list-item-title {{ item.title }}
       v-divider
-      v-list-item(@click="$auth.logout()")
+      v-list-item(@click="logout")
         v-list-item-action
           v-icon mdi-logout
         v-list-item-content
@@ -30,28 +30,50 @@ export default {
       fixed: false,
       items: [
         {
-          header: 'Administración',
-        },
-        {
           icon: 'mdi-account-group',
           title: 'Usuarios',
           name: 'users',
+          roles: ['admin'],
         },
         {
           icon: 'mdi-clipboard-list-outline',
           title: 'Inventario',
           name: 'books',
+          roles: ['admin'],
         },
         {
-          header: 'Lector',
+          icon: 'mdi-information-outline',
+          title: 'Gestión de préstamos',
+          name: 'loans',
+          roles: ['admin', 'bibliotecario'],
+        },
+        {
+          icon: 'mdi-view-list',
+          title: 'Mis préstamos',
+          name: 'myloans',
+          roles: ['admin', 'bibliotecario', 'lector'],
         },
         {
           icon: 'mdi-magnify',
           title: 'Búsqueda de libros',
           name: 'index',
+          roles: ['admin', 'bibliotecario', 'lector'],
         },
       ],
     }
+  },
+  computed: {
+    menu() {
+      return this.items.filter((item) => {
+        return item.roles.includes(this.$auth.user.role)
+      })
+    },
+  },
+  methods: {
+    logout() {
+      this.$auth.logout()
+      this.$router.push('/login')
+    },
   },
 }
 </script>
